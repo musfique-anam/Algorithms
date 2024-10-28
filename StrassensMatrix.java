aaecfdf4a34c882424843b9491a6bfd3aa724621
musfique-anam <musfique2002.personal@gmail.com>
2024-10-28 23:16:07 +0600

    matrix

--- StrassensMatrix.java
+++ StrassensMatrix.java
@@ -31,4 +31,14 @@ public class StrassensMatrix {
         return result;
     }
 
+    public static void printMatrix (int [][] matrix) {
+        for (int i=0;i < matrix.length;i++) {
+            for (int j =0;j<matrix[i].length;j++){
+                System.out.print(matrix [i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
