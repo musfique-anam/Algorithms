f3c651acfb2e50258826608279d5bc7bd96eaf50
musfique-anam <musfique2002.personal@gmail.com>
2024-10-30 20:17:15 +0600

    MST

--- PrimsMST.java
+++ PrimsMST.java
@@ -15,7 +15,29 @@ class PrimsMST {
 
         for (int count =0;count < V-1; count++) {
             int u =minKey(key, mstSet);
+            mstSet[u] =true;
+
+            for (int v=0;v < V; v++) {
+                if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                    parent[v] =u;
+                    key[v]=graph[u][v];
+                }
+            }
+        }
+        for (int i=1;i<V;i++) {
+            System.out.println(parent[i]+" - "+i+"\t"+graph[i][parent[i]]);
         }
     }
+    int minKey(int[] key,boolean[] mstSet) {
+        int min =Integer.MAX_VALUE, minIndex=-1;
+        for(int v=0;v<V;v++) {
+            if(!mstSet[v] && key[v] < min) {
+                min=key[v];
+                minIndex=v;
+            }
+        }
+        return minIndex;
+    }
+
     
 }
