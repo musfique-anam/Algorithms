4e9f84dc3a60239f952f252a30a2115982d537bb
musfique-anam <musfique2002.personal@gmail.com>
2024-11-03 22:16:02 +0600

    Graph Traversal

--- BFS.java
+++ BFS.java
@@ -1,36 +1,37 @@
 import java.util.*;
 
-class BFS {
-
-    static void bfs(List<List<Integer>> adj, int s) {
-        Queue<Integer> q =new LinkedList<>();
+public class BFS {
+    static void bfs(List<List<Integer>>adj,int s) {
+        Queue<Integer> q=new LinkedList<>();
         boolean[] visited =new boolean[adj.size()];
-        visited[s] =true;
+        visited[s]=true;
         q.add(s);
 
-        while (!q.isEmpty()) {
-            int curr =q.poll();
-            System.out.print(curr + " ");
-
-            for (int neighbor : adj.get(curr)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] =true;
-                    q.add(neighbor);
-                }
+        while(!q.isEmpty()) {
+            int curr=q.poll();
+            System.out.print(curr +" ");
+        
+        for(int x:adj.get(curr)) {
+            if (!visited[x]) {
+                visited[x]=true;
+                q.add(x);
             }
         }
     }
+}
+        
 
-    static void addEdge(List<List<Integer>> adj,int u,int v){
+    static void addEdge (List<List<Integer>> adj,int u,int v) {
         adj.get(u).add(v);
         adj.get(v).add(u);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int V=5;
-        List<List<Integer>> adj = new ArrayList<>(V);
-        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
-
+        List<List<Integer>> adj=new ArrayList<>(V);
+        for (int i=0;i<V;i++){
+            adj.add(new ArrayList<>());
+        }
         addEdge(adj,0,1);
         addEdge(adj,0,2);
         addEdge(adj,1,3);
@@ -39,6 +40,7 @@ class BFS {
 
         System.out.println("BFS starting from 0:");
         bfs(adj,0);
-
+        
     }
 }
+
