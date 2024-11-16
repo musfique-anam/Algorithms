5f3534c98e6ea3fe1f63f2c4cf3d8b82724ded49
musfique-anam <musfique2002.personal@gmail.com>
2024-11-16 20:33:49 +0600

    Fractional KnapSack

--- FracKnapsack.java
+++ FracKnapsack.java
@@ -1,33 +1,36 @@
 public class FracKnapsack {
     static int n = 5;
-    static int p[] = {3,3,2,5,1};
-    static int w[] = {10,15,10,12,8};
+    static int[] p = {3, 3, 2, 5, 1};
+    static int[] w = {10, 15, 10, 12, 8};
     static int W = 10;
+
     public static void main(String[] args) {
-        int cur_w;
+        int cur_w = W;
         float tot_v = 0;
-        int i, maxi;
-        int used[] = new int [10];
-        for (i =0;i < n; ++i)
-        used[i] =0;
-        cur_w =W;
+        int maxi;
+        int[] used = new int[n];
+        
+        for (int i = 0; i < n; ++i) {
+            used[i] = 0;
+        }
+        
         while (cur_w > 0) {
             maxi = -1;
-            for (i = 0; i < n; ++i)
-            if ((used[i] == 0) && 
-            ((maxi == -1) || ((float)w[i]/p[i] > (float)w[maxi]/p[maxi])))
-            maxi = i;
+            for (int i = 0; i < n; ++i) {
+                if (used[i] == 0 && (maxi == -1 || (float) w[i] / p[i] > (float) w[maxi] / p[maxi])) {
+                    maxi = i;
+                }
+            }
             used[maxi] = 1;
-            cur_w -=p[maxi];
+            cur_w -= p[maxi];
             tot_v += w[maxi];
-            if (cur_w >= 0)
-                System.out.println("Added object " + maxi + 1 + " (" + w[maxi] + "," + p[maxi] + ") completely in the bag. Space left: " + cur_w);
-            else {
-                System.out.println("Added " + ((int)((1 + (float)cur_w/p[maxi]) * 100)) + "% (" + w[maxi] + "," + p[maxi] + ") of object " + (maxi + 1) + " in the bag.");
+            
+            if (cur_w < 0) {
                 tot_v -= w[maxi];
-                tot_v +=(1+ (float)cur_w/p[maxi]) * w[maxi];
+                tot_v += (1 + (float) cur_w / p[maxi]) * w[maxi];
             }
         }
-        System.out.println("Filled the bag with objects worth " +tot_v);
+        
+        System.out.println("Maximum total value obtained: " + tot_v);
     }
 }
